87e469995fc19af84e42a1394c14ababa596d9ba
minhdang1202 <vuminhdang20@gmail.com>
2021-06-28 17:38:50 +0700

    9

--- src/component/search/Search.js
+++ src/component/search/Search.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Search.css'
 import { Route } from 'react-router-dom'
 import SearchFilm from './SearchFilm'
-const Search = () => {
+const Search = (props) => {
    return (
         <>
             <Route path='/timkiem/:name' component = {SearchFilm} />
